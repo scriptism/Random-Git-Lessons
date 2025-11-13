e537319e79130631a8755e3b203713cc6bccffdb
Amin <aminullahpuya@gmail.com>
2025-11-13 15:10:13 +0100

    final update

---
+++ git-script.js
@@ -0,0 +1,12 @@
+function openTab(evt, tabName) {
+  const tabcontent = document.getElementsByClassName("tab-content");
+  for (var i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  const tabbuttons = document.getElementsByClassName("tab-button");
+  for (var i = 0; i < tabbuttons.length; i++) {
+    tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
